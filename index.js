924ede177206aa91f031a876f1775cf166defdf2
unknown <shirleyhaddix82@outlook.com>
2025-06-27 23:31:28 -0400

    Working on GitHub API

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-async function githubFetch() {
-    const response = await fetch('https://api.github.com/users/drwatson0814/repos');
-    const data = await response.json(); 
+const userRepos = document.getElementsByClassName('projects-icon').addEventListener('DOMContentLoaded', () => {
+const GITHUB_USERNAME = 'DocWatson0814';
+const Personal Access Token = '';
+});
+
+
+
+const repoListA = async () => {
+const endpoint = 'https://api.github.com/users/DrWatson0814/repos';
+    try {
+        const response = await fetch('https://api.github.com/users/drwatson0814/repos');
+        const data = await response.json(); 
+
+    }
      
 }
 
 
-githubFetch();
+// function populateList(obj) {
+
+
+// }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
