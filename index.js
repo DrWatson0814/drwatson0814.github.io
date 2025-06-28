75c12c693c4b7c95849a806ae38a75ca18a59677
unknown <shirleyhaddix82@outlook.com>
2025-06-28 18:46:09 -0400

    Almost have the js licked. WORKING: Getting CSS for progect.html squashed

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const userRepos = document.getElementsByClassName('projects-icon').addEventListener('DOMContentLoaded', () => {
+document.getElementById('repoList');
+const userRepos = document.addEventListener('DOMContentLoaded', () => {
 const GITHUB_USERNAME = 'DocWatson0814';
-const Personal Access Token = '';
+const personalAccessToken = '';
 
 
 
 const repoListA = async () => {
-const endpoint = 'https://api.github.com/users/DrWatson0814/repos';
+const endpoint = 'https://api.github.com/users/drwatson0814/repos';
 try {
         const response = await fetch('https://api.github.com/users/drwatson0814/repos');
         const data = await response.json();
+        const repoContainer = document.getElementById('repoList')
         
-        for(const repo in data) {
+        for(const repo of data) {
             const repoInfo = document.createElement('div');
             const repoName = document.createElement('h2');
             const repoLink = document.createElement('a');
-            repoLink.classList.add('repoInfo');
-            repoLink.href = endpoint;
+            repoLink.classList.add('repo-info');
+            repoLink.href = repo.html_url;
             repoLink.textContent = repo.name;
             repoName.appendChild('repoLink');
             repoInfo.appendChild('repoName');
+            repoContainer.appendChild('repoInfo');
         };
     }catch(error) {
         console.error(error);
