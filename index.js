d1a2f06e57ca85e8f8c7e273a28c47436bcab8c6
unknown <shirleyhaddix82@outlook.com>
2025-06-28 00:17:01 -0400

    Maybe got js wrote

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const userRepos = document.getElementsByClassName('projects-icon').addEventListener('DOMContentLoaded', () => {
 const GITHUB_USERNAME = 'DocWatson0814';
 const Personal Access Token = '';
-});
 
 
 
 const repoListA = async () => {
 const endpoint = 'https://api.github.com/users/DrWatson0814/repos';
-    try {
+try {
         const response = await fetch('https://api.github.com/users/drwatson0814/repos');
-        const data = await response.json(); 
-
+        const data = await response.json();
+        
+        for(const repo in data) {
+            const repoInfo = document.createElement('div');
+            const repoName = document.createElement('h2');
+            const repoLink = document.createElement('a');
+            repoLink.classList.add('repoInfo');
+            repoLink.href = endpoint;
+            repoLink.textContent = repo.name;
+            repoName.appendChild('repoLink');
+            repoInfo.appendChild('repoName');
+        };
+    }catch(error) {
+        console.error(error);
     }
-     
-}
+    
+};
+
+repoListA();
+
+});
+
+
 
 
 // function populateList(obj) {
